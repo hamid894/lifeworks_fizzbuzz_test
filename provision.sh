1f813b9bac86fb89f9c76fe8f3a80f9cd2ab71d4
Hamid Haghayegh <hamid.haghayegh@gmail.com>
2018-04-22 14:04:10 +0100

    removing unnessary bash file

--- provision.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-yum -y update > /dev/null 2>&1
-yum -y install httpd > /dev/null 2>&1
-service httpd start
-rm -rf /var/www
-mkdir /var/www
-ln -s /vagrant /var/www/html
-echo "hello world!" > /var/www/html/index.html
